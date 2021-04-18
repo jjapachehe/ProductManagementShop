61319c2de7b51ebf7e7b3e51029f23e26021b8ea
Jhon Jairo Apache Hernandez <jho_ja8@hotmail.com>
2021-04-17 22:25:38 -0500

    Food and Drink classes added

---
+++ ProductManagement/src/labs/pm/data/Food.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2021 ejhoapa
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import labs.pm.app.Rating;
+
+/**
+ *
+ * @author ejhoapa
+ */
+public class Food extends Product{
+
+    public Food(int id, String name, BigDecimal price, Rating rating, LocalDate bestBefore) {
+        super(id, name, price, rating);
+        this.bestBefore = bestBefore;
+    }
+        
+    private LocalDate bestBefore;
+
+    /**
+     * Get the value of best before date for the product
+     *
+     * @return the value of bestBefore
+     */
+    public LocalDate getBestBefore() {
+        return bestBefore;
+    }
+
+}
